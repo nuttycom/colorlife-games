7b83542a1fbef228b3c2611d62e0d3fa7b284199
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-03-22 13:01:57 -0600

    Game mechanics more or less done; just need to wire in the population to the display.

--- src/main/scala/org/nuttycombe/colorlife/ce/ColonizeEarth.scala
+++ src/main/scala/org/nuttycombe/colorlife/ce/ColonizeEarth.scala
@@ -15,15 +15,18 @@ import scala.annotation.tailrec
 
 object ColonizeEarth extends Life2DToroidal with ColorLife with GUI {
   override val gameTitle = "ColorLife: Colonize Earth!"
-  override val xsize = 40
+  override val xsize = 41
   override val ysize = 35
 
   val earthSize = 3
 
+  val xcenter = xsize/2.0f
+  val ycenter = ysize/2.0f
+  val earthHalfWidth = earthSize/2.0f
+
   def main(argv: Array[String]) {
-    val players = getPlayers
-
-
+    drawBoard
+    play(getPlayers, Map())
   }
 
   def getPlayers : List[Player] = {
@@ -39,19 +42,36 @@ object ColonizeEarth extends Life2DToroidal with ColorLife with GUI {
   }
 
   @tailrec def play(players: List[Player], pop: Population): Population = {
-    play(players, generation(round(players.map(v => (random, v)).sortBy(_._1).map(_._2), pop)))
+    winner(players, pop) match {
+      case Some(w) => pop
+      case None => play(players, generation(cycle(players.map(v => (random, v)).sortBy(_._1).map(_._2), pop)))
+    }
   }
 
-  @tailrec def round(players: List[Player], pop: Population): Population = {
+  @tailrec def cycle(players: List[Player], pop: Population): Population = {
     players match {
-      case p :: xs => round(xs, p.turn(pop))
+      case p :: xs => cycle(xs, p.turn(pop))
       case Nil => pop
     }
   }
 
-  def isEarthCell(x: Int, y: Int) = abs(x - xsize/2f) < earthSize/2f && abs(y - xsize/2f) < earthSize/2f
+  val earthLocations: Set[Location] = {
+    (
+      for {
+        x <- round(xcenter - earthHalfWidth) until round(xcenter + earthHalfWidth)
+        y <- round(ycenter - earthHalfWidth) until round(ycenter + earthHalfWidth)
+      } yield (x, y)
+    ).toSet
+  }
 
-  override def createCellFace(x: Int, y: Int) = if (isEarthCell(x, y)) new CellFace(x, y, Color.WHITE) else new CellFace(x, y)
+  def inExclusionZone(x: Int, y: Int) = abs(x - xcenter) < (earthHalfWidth + 1) && abs(y - ycenter) < (earthHalfWidth + 1)
+
+  override def createCellFace(x: Int, y: Int) = if (earthLocations.contains((x, y))) new CellFace(x, y, Color.WHITE) else new CellFace(x, y)
+
+  def winner(players: List[Player], pop: Population): Option[Player] = {
+    val earthColors = pop.filterKeys(earthLocations).values.toSet
+    if (earthColors.size == 1) players.find(_.color == earthColors.head) else None
+  }
 
   case class Player(name: String, color: Color) {
     private var seeds : Int = 0
@@ -80,18 +100,17 @@ object ColonizeEarth extends Life2DToroidal with ColorLife with GUI {
           override def keyPressed(ev:KeyEvent)  = ()
           override def keyReleased(ev:KeyEvent) = ()
           override def keyTyped(ev:KeyEvent) = ev.getKeyChar match {
-            case ' ' => if (seeds > 0 && !pop.contains((userx, usery))) {
+            case ' ' if seeds > 0 && !pop.contains((userx, usery)) && !inExclusionZone(userx, usery) =>
               seeds -= 1
               population = pop + ((userx, usery) -> color)
-            }
 
-            case n if (1 until 8).contains(n.asDigit) => {
+            case n if (1 until 8).contains(n.asDigit) => 
               journeyLength = n
               journeyRemaining = n
               endTurn
-            }
 
-            case '\r' => endTurn
+            case '\r' =>
+              endTurn
 
             case _ => ()
           }
@@ -107,6 +126,5 @@ object ColonizeEarth extends Life2DToroidal with ColorLife with GUI {
         population
       }
     }
-
   }
 }
